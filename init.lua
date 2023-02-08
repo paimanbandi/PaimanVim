6ca2885cb2077fcee55192b18e14fc8989068378
Paiman <paiman.bandi@gmail.com>
2023-02-08 23:28:40 +0700

    add plugin nvim-ufo

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@ require('packer').startup(function(use)
 
   use('akinsho/toggleterm.nvim')
 
+  use { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' }
+
   use('tveskag/nvim-blame-line')
 
   use({
@@ -235,6 +237,44 @@ require("toggleterm").setup({
   close_on_exit = true,
   shell = vim.o.shell,
 })
+
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+-- Option 2: nvim lsp as LSP client
+-- Tell the server the capability of foldingRange,
+-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true
+}
+local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+for _, ls in ipairs(language_servers) do
+  require('lspconfig')[ls].setup({
+    capabilities = capabilities
+    -- you can add other fields for setting up lsp server in this table
+  })
+end
+require('ufo').setup()
+--
+
+-- Option 3: treesitter as a main provider instead
+-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
+-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+-- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+-- require('ufo').setup({
+--   provider_selector = function(bufnr, filetype, buftype)
+--     return { 'treesitter', 'indent' }
+--   end
+-- })
+--
 
 require('lualine').setup {
   options = {
@@ -453,5 +493,4 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
-}
 }
